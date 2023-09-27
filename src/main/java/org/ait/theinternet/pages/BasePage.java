c87e7953aa484061aef8eb117521d1034ed730e8
Yuliia Tsaruk <tsarukyula87@gmail.com>
2023-09-27 23:37:45 +0200

    added BrokenImagesTests

--- src/main/java/org/ait/theinternet/pages/BasePage.java
+++ src/main/java/org/ait/theinternet/pages/BasePage.java
@@ -4,6 +4,12 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.time.Duration;
 
 public abstract class BasePage {
 
@@ -26,17 +32,33 @@ public abstract class BasePage {
         }
     }
 
-    public void clickWithJSExecutor(WebElement element, int x, int y) {
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("window.scrollBy(" + x + "," + y + ")");
-        element.click();
+
+    public boolean shouldHaveText(WebElement element, String text, int time) {
+        return new WebDriverWait(driver, Duration.ofSeconds(time))
+                .until(ExpectedConditions.textToBePresentInElement(element, text));
     }
 
-    public void typeWithJSExecutor(WebElement element, String text, int x, int y) {
-        if (text != null) {
-            clickWithJSExecutor(element, x, y);
-            element.clear();
-            element.sendKeys(text);
+    public boolean isTextPresent(WebElement element,String book) {
+        return element.getText().contains(book);
+    }
+
+    public void verifyLinks(String linkUrl) {
+        try {
+            URL url = new URL(linkUrl);
+
+            // create connection
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setConnectTimeout(5000);
+            connection.connect();
+
+            // get response status code
+            if (connection.getResponseCode() >= 400) {
+                System.out.println(linkUrl + " - " + connection.getResponseMessage() + " is a broken link");
+            } else {
+                System.out.println(linkUrl + " - " + connection.getResponseMessage());
+            }
+        } catch (Exception ex) {
+            System.out.println(linkUrl + " - " + ex.getMessage() + " is a broken link");
         }
     }
 
--- src/test/java/org/ait/theinternet/tests/TestBase.java
+++ src/test/java/org/ait/theinternet/tests/TestBase.java
@@ -9,7 +9,7 @@ import java.time.Duration;
 
 public class TestBase {
 
-    WebDriver driver;
+    public WebDriver driver;
 
     @BeforeMethod
     public void init() {
